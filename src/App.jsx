f3a131009ae02e3401ddea9527ac4e30201a7323
Rhythmdoshi31 <rhythmdoshi04@gmail.com>
2025-03-14 20:09:04 +0530

    applied glassmorphism to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -14,24 +14,26 @@ import Contactus from './Components/Contactus'
 
 function App() {
   return (
-    <div className='w-screen h-screen bg-[#1F1E24] text-white flex'>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/trending' element={<Trending />}/>
-        <Route path='/popular' element={<Popular />}/>
-        <Route path='/movies' element={<Movie />} />
-        <Route path='/movie/details/:id' element={<MovieDetails />}>
-          <Route path='Trailer' element={<Trailer />}/>
-        </Route>
-        <Route path='/tvshows' element={<TVshows />} />
-        <Route path='/tv/details/:id' element={<TvDetails />}>
-          <Route path='Trailer' element={<Trailer />}/>
-        </Route>
-        <Route path='/people' element={<People />} />
-        <Route path='/people/details/:id' element={<PersonDetails />}/>
-        <Route path='/whoweare' element={<Whoweare />}/>
-        <Route path='/contactus' element={<Contactus />}/>
-      </Routes>
+    <div className='w-screen h-screen bg-[url("/glassy.jpg")] text-white flex'>
+      <div className='w-screen h-screen bg-black/70 backdrop-blur-lg flex'>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/trending' element={<Trending />}/>
+          <Route path='/popular' element={<Popular />}/>
+          <Route path='/movies' element={<Movie />} />
+          <Route path='/movie/details/:id' element={<MovieDetails />}>
+            <Route path='Trailer' element={<Trailer />}/>
+          </Route>
+          <Route path='/tvshows' element={<TVshows />} />
+          <Route path='/tv/details/:id' element={<TvDetails />}>
+            <Route path='Trailer' element={<Trailer />}/>
+          </Route>
+          <Route path='/people' element={<People />} />
+          <Route path='/people/details/:id' element={<PersonDetails />}/>
+          <Route path='/whoweare' element={<Whoweare />}/>
+          <Route path='/contactus' element={<Contactus />}/>
+        </Routes>
+      </div>
     </div>
   )
 }
